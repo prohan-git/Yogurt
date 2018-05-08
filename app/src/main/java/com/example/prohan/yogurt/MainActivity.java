41bd6a4fe85529a815076368c41405c0e2e7fb7d
tijun <qqgeshe@163.com>
2018-05-09 00:53:06 +0800

    update .gitignore

---
+++ app/src/main/java/com/example/prohan/yogurt/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.prohan.yogurt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);//test
+        setContentView(R.layout.activity_main);
+    }
+}
