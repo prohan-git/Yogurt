fb53c34141330a9306fda505ac4edfc5f6b7e920
TJun <prohankj@outlook.com>
2018-06-20 13:23:15 +0800

    commit

---
+++ app/src/main/java/com/example/proha/yogurt/HomeActivity.kt
@@ -0,0 +1,120 @@
+package com.example.proha.yogurt
+
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.view.LayoutInflater
+
+import kotlinx.android.synthetic.main.activity_home.*
+import android.support.design.internal.BottomNavigationItemView
+import android.support.design.internal.BottomNavigationMenuView
+import android.support.v4.view.ViewPager
+import android.widget.TextView
+import com.example.proha.yogurt.module.home.MainFragment
+
+
+class HomeActivity : AppCompatActivity(), MainFragment.OnFragmentInteractionListener {
+
+    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        when (it.itemId) {
+            R.id.navigation_home -> {
+                container.setCurrentItem(0, false)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                container.setCurrentItem(1, false)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                container.setCurrentItem(2, false)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)//设置viewpager的adapter
+        container.adapter = mSectionsPagerAdapter
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        container.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                navigation.selectedItemId = when (position) {
+                    0 -> R.id.navigation_home
+                    1 -> R.id.navigation_dashboard
+                    2 -> R.id.navigation_notifications
+                    else -> 0
+                }
+
+            }
+        })
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    /**
+     * 添加角标
+     */
+    fun addBadge(index: Int, string: String) {
+        val bottomNavigationMenuView: BottomNavigationMenuView = navigation.getChildAt(0) as BottomNavigationMenuView
+        val itemView = bottomNavigationMenuView.getChildAt(index) as BottomNavigationItemView
+        val badge = LayoutInflater.from(this)
+                .inflate(R.layout.badge, bottomNavigationMenuView, false)
+        findViewById<TextView>(R.id.msg).text = string
+        itemView.addView(badge)
+    }
+
+    /**
+     * 删除角标
+     */
+    fun removeBadge(navigationView: BottomNavigationView, index: Int) {
+        val bottomNavigationMenuView = navigationView.getChildAt(0) as BottomNavigationMenuView
+        val v = bottomNavigationMenuView.getChildAt(index)
+        val itemView = v as BottomNavigationItemView
+        itemView.removeViewAt(itemView.childCount - 1)
+    }
+
+    /**
+     * viewPagerAdapter类
+     */
+    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+
+            return when (position) {
+                0 -> MainFragment.newInstance()
+                1 -> MainFragment.newInstance()
+                2 -> MainFragment.newInstance()
+                else -> MainFragment.newInstance()
+            }
+
+
+        }
+
+        override fun getCount(): Int {
+            return 3
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/proha/yogurt/module/home/MainFragment.kt
@@ -0,0 +1,81 @@
+package com.example.proha.yogurt.module.home
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.proha.yogurt.R
+
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [MainFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class MainFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+                MainFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, "0")
+                        putString(ARG_PARAM2, "0")
+                    }
+                }
+    }
+
+
+}
