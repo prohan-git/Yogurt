86b822d93a0d931cc40a3acc98f5ebf5cc3244c0
TJun <prohankj@outlook.com>
2018-07-13 10:19:39 +0800

    format code

--- app/src/main/java/com/example/proha/yogurt/module/home/MainFragment.kt
+++ app/src/main/java/com/example/proha/yogurt/module/home/MainFragment.kt
@@ -1,28 +1,13 @@
 package com.example.proha.yogurt.module.home
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.example.proha.yogurt.R
 
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [MainFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class MainFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
